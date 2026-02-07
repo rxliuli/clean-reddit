46c0c692f704594a69e4736e462c1affe8556b13
rxliuli <rxliuli@gmail.com>
2026-02-07 13:29:54 +0800

    feat: update gecko id

--- wxt.config.ts
+++ wxt.config.ts
@@ -39,9 +39,7 @@ export default defineConfig({
     if (env.browser === 'firefox') {
       manifest.browser_specific_settings = {
         gecko: {
-          id:
-            manifest.name!.toLowerCase().replaceAll(/[^a-z0-9]/g, '-') +
-            '@rxliuli.com',
+          id: 'clean-reddit@rxliuli.com',
           // @ts-expect-error https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json/browser_specific_settings#data_collection_permissions
           data_collection_permissions: {
             required: ['none'],
